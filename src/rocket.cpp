647ca685ad2655bad51ed2e10415a270eae4ac57
tcohenkl <tal.ck40@gmail.com>
2023-09-04 00:48:13 -0400

    change rotation speed

--- src/rocket.cpp
+++ src/rocket.cpp
@@ -47,7 +47,7 @@ CollisionDetail Rocket::checkCollision(const sf::Sprite& planetSprite) {
 
 
 Rocket::Rocket() : 
-rotationSpeed(0.125f), 
+rotationSpeed(0.14f), 
 acceleration(0.00002f), 
 maxVelocity(0.30f), // originally 0.20
 isThrusting(false),
