d714d003dcd949ded8a512f563940995085a81a6
tcohenkl <tal.ck40@gmail.com>
2023-09-05 17:50:18 -0400

    add readme

--- src/Collision.h
+++ src/Collision.h
@@ -9,7 +9,7 @@
 Notice from the original version:
 
 (c) 2009 - LittleMonkey Ltd
- 
+ c
 This software is provided 'as-is', without any express or
 implied warranty. In no event will the authors be held
 liable for any damages arising from the use of this software.
